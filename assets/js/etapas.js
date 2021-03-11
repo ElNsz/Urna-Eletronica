e0a17d29521b450f5df481124b80ce8b75ce9066
Ellivelton Narciso <ellivelton.souza@gmail.com>
2021-03-11 02:53:21 -0300

    Feito o array básico dos canditados e manipulação da estrutura da urna.

---
+++ assets/js/etapas.js
@@ -0,0 +1,70 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        canditados: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {
+                        url: './../../img/38111.jpg',
+                        legenda: 'Vereador'
+                    }
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEF6',
+                fotos:[
+                    {
+                        url:'./../../img/77222.jpg',
+                        legenda: 'Vereador'
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEFITO',
+        numeros: 2,
+        canditados: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {
+                        url: './../../img/99.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: './../../img/99_2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'TR',
+                vice: 'Zuka',
+                fotos:[
+                    {
+                        url:'./../../img/84.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: './../../img/84_2.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            }
+        ]
+    }
+]
