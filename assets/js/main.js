41dd0cffc9b57928000afdd2566338ee32e1be67
Ellivelton Narciso <ellivelton.souza@gmail.com>
2021-03-15 10:28:59 -0300

    Preenchendo interface com números.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,62 @@ let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-1-4');
 let lateral = document.querySelector('.d-1--direita');
 let numeros = document.querySelector('.d-1-3');
+
+// Variáveis de controle de ambiente
+
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+
+    for(let i=0;i<etapa.numeros;i++) {
+        if(i === 0 ){
+            numeroHtml += '<div class="numero pisca"></div>'
+        } else {
+            numeroHtml += '<div class="numero"></div>';
+        }
+       
+    };
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+};
+function atualizaInterface() {
+    
+}
+
+// Funções de click 
+
+function clicou(n) {
+    let elNumero = document.querySelector('.numero.pisca');
+    if(elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `$(numero)$(n)`;
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+            atualizaInterface();
+        }
+        
+    };
+};
+function branco() {
+    
+};
+function corrige() {
+    
+};
+function confirma() {
+    
+};
+
+comecarEtapa();
